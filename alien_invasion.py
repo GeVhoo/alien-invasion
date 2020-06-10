abdb0f98ba058f2e902f494d16c544c65835b1b9
GeVhoo <gvahrushev@mail.ru>
2020-06-10 17:36:44 +0500

    Add speedup scale

--- alien_invasion.py
+++ alien_invasion.py
@@ -68,6 +68,9 @@ class AlienInvasion:
         """Запускает новыую игру при нажатии кнопки Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Сброс игровых настроек.
+            self.settings.initialize_dynamic_settings()
+
             # Сброс игровой статистики.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -129,6 +132,7 @@ class AlienInvasion:
             # Уничтожение существующих снарядов и создание нового флота.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """Обновляет позиции всех пришельцев во флоте."""
